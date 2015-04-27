64dac4a3e1745ba91ace35e1a7f792a81ead7524
SharpWave <sullivan.dw@gmail.com>
2015-04-27 14:42:30 -0400

    reads and does temporary write, doesn't do final write

--- DWS_WidebandPlx2dat.m
+++ DWS_WidebandPlx2dat.m
@@ -16,24 +16,27 @@ CurrStart = ChunkSize*10;
 ad = zeros(NumChannels,ChunkSize);
 
 for i = 1:NumChannels
+  i
   [~,~,~,~,v] = plx_ad(inname,i-1);
   fouts{i} = fopen(['tmpchan.',int2str(i-1)],'w');
-  
-for j = 1:NumFullChunks
-    tic
-    j/NumFullChunks
-    for i = 1:NumChannels
-        
-        % read the wideband signal for channel i
-        [~,~,ad(i,1:ChunkSize)] = plx_ad_span(inname,i-1,CurrStart,CurrStart+ChunkSize-1);
-        %ad(i,:) = temp;
-        
-    end
-    CurrStart = CurrStart + ChunkSize;
-    fwrite(fout,ad,'int16');
-    toc
+  fwrite(fouts{i},ad,'int16');
 end
-fclose(fout);
+  
+% for j = 1:NumFullChunks
+%     tic
+%     j/NumFullChunks
+%     for i = 1:NumChannels
+%         
+%         % read the wideband signal for channel i
+%         [~,~,ad(i,1:ChunkSize)] = plx_ad_span(inname,i-1,CurrStart,CurrStart+ChunkSize-1);
+%         %ad(i,:) = temp;
+%         
+%     end
+%     CurrStart = CurrStart + ChunkSize;
+%     fwrite(fout,ad,'int16');
+%     toc
+% end
+% fclose(fout);
 
 
 
