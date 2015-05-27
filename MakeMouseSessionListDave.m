49877796ab6cd4a7578552a9011ea86b21a8821f
SharpWave <sullivan.dw@gmail.com>
2015-05-27 16:09:18 -0400

    new database script moved unfuck traces here

---
+++ MakeMouseSessionListDave.m
@@ -0,0 +1,129 @@
+function [] = MakeMouseSessionListDave()
+% this function makes a list of the location of all sessions on disk
+
+i = 1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '09-29-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Square Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\09_29_2014\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-01-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Square Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_01_2014\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-03-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Square Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_03_2014\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-06-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Home Cage';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_06_2014\1 - homecage 201B\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-06-2014';
+MD(i).Session = 2;
+MD(i).Env = 'Triangle Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_06_2014\2 - triangle track 201B\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-08-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Triangle Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_08_2014\1 - triangle track 201B\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-08-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Triangle Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_08_2014\1 - triangle track 201B\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-08-2014';
+MD(i).Session = 2;
+MD(i).Env = 'Home Cage';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_08_2014\2 - homecage 201B\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-10-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Triangle Track';
+MD(i).Room = '2 Cu 201A';
+MD(i).Location = 'E:\GC6f_31\10_10_2014\1 - triangle track 201A\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-10-2014';
+MD(i).Session = 2;
+MD(i).Env = 'Triangle Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_10_2014\2 - triangle track 201B\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-14-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Triangle Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_14_2014\1 - triangle track 201B\working';
+MD(i).Notes = [];
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-14-2014';
+MD(i).Session = 2;
+MD(i).Env = 'Triangle Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_14_2014\2 - triangle track rotated plus 120 201B\working'
+MD(i).Notes = 'rotated +120 degrees';
+
+i = i+1;
+MD(i).Animal = 'GCamp6f_31';
+MD(i).Date = '10-17-2014';
+MD(i).Session = 1;
+MD(i).Env = 'Triangle Track';
+MD(i).Room = '2 Cu 201B';
+MD(i).Location = 'E:\GC6f_31\10_14_2014\1 - triangle track 201B\working';
+MD(i).Notes = [];
+
+
+
+
+
+
+
+
+
+
