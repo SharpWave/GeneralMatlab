e137f635e9242efa33ec13b43f49cfeb031e2206
SharpWave <sullivan.dw@gmail.com>
2015-06-01 13:02:42 -0400

    updates to sessions

--- ChangeDirectory.m
+++ ChangeDirectory.m
@@ -1,4 +1,4 @@
-function [] = ChangeDirectory(sess_date,sess_num)
+function [dirstr] = ChangeDirectory(animal_id,sess_date,sess_num)
 
 if(~exist('sess_num'))
     sess_num = 1;
@@ -6,18 +6,20 @@ end
 
 CurrDir = pwd;
 
-MasterDirectory = 'E:\MasterData';
+MasterDirectory = 'C:\MasterData';
 cd(MasterDirectory);
 
-load MD.mat;
+load MasterDirectory.mat;
 
 cd(CurrDir);
 
 NumEntries = length(MD);
 
 for i = 1:NumEntries
-    if (strcmp(MD(i).Date,'sess_date') & MD(i).Session == sess_num)
+
+    if (strcmp(MD(i).Date,sess_date) & (MD(i).Session == sess_num) & strcmp(MD(i).Animal,animal_id))
         cd(MD(i).Location);
+        dirstr = (MD(i).Location);
         return;
     end
 end
