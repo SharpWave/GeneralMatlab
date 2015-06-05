b2c6c8aaafc96263ffbe0a296d4ac77ae478cf4b
SharpWave <sullivan.dw@gmail.com>
2015-06-05 16:19:53 -0400

    made changes to reflect full animal name in directory structure

--- MakeMouseSessionList.m
+++ MakeMouseSessionList.m
@@ -13,7 +13,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Square Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\09_29_2014\working';
+    MD(i).Location = 'E:\GCaMP6f_31\09_29_2014\working';
 end
 MD(i).Notes = [];
 
@@ -24,7 +24,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Square Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_01_2014\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_01_2014\working';
 end
 MD(i).Notes = [];
 
@@ -35,7 +35,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Square Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_03_2014\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_03_2014\working';
 end
 MD(i).Notes = [];
 
@@ -46,7 +46,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Home Cage';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_06_2014\1 - homecage 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_06_2014\1 - homecage 201B\working';
 end
 MD(i).Notes = [];
 
@@ -57,7 +57,7 @@ MD(i).Session = 2;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_06_2014\2 - triangle track 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_06_2014\2 - triangle track 201B\working';
 end
 MD(i).Notes = [];
 
@@ -68,7 +68,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_08_2014\1 - triangle track 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_08_2014\1 - triangle track 201B\working';
 end
 MD(i).Notes = [];
 
@@ -79,7 +79,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_08_2014\1 - triangle track 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_08_2014\1 - triangle track 201B\working';
 end
 MD(i).Notes = [];
 
@@ -90,7 +90,7 @@ MD(i).Session = 2;
 MD(i).Env = 'Home Cage';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_08_2014\2 - homecage 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_08_2014\2 - homecage 201B\working';
 end
 MD(i).Notes = [];
 
@@ -101,7 +101,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201A';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_10_2014\1 - triangle track 201A\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_10_2014\1 - triangle track 201A\working';
 end
 MD(i).Notes = [];
 
@@ -112,7 +112,7 @@ MD(i).Session = 2;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_10_2014\2 - triangle track 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_10_2014\2 - triangle track 201B\working';
 end
 MD(i).Notes = [];
 
@@ -123,7 +123,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_14_2014\1 - triangle track 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_14_2014\1 - triangle track 201B\working';
 end
 MD(i).Notes = [];
 
@@ -134,7 +134,7 @@ MD(i).Session = 2;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_14_2014\2 - triangle track rotated plus 120 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_14_2014\2 - triangle track rotated plus 120 201B\working';
 end
 MD(i).Notes = 'rotated +120 degrees';
 
@@ -145,7 +145,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_17_2014\1 - triangle track 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_17_2014\1 - triangle track 201B\working';
 end
 MD(i).Notes = [];
 
@@ -156,7 +156,7 @@ MD(i).Session = 2;
 MD(i).Env = 'Triangle Open Field';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_17_2014\2 - triangle open-field 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_17_2014\2 - triangle open-field 201B\working';
 end
 MD(i).Notes = [];
 
@@ -167,7 +167,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Triangle Open Field';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_20_2014\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_20_2014\working';
 end
 MD(i).Notes = [];
 
@@ -178,7 +178,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Triangle Track';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_22_2014\1 - triangle track 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_22_2014\1 - triangle track 201B\working';
 end
 MD(i).Notes = [];
 
@@ -189,7 +189,7 @@ MD(i).Session = 2;
 MD(i).Env = 'Triangle Open Field';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_22_2014\2 - triangle 201B\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_22_2014\2 - triangle 201B\working';
 end
 MD(i).Notes = [];
 
@@ -200,7 +200,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Square Open Field';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_24_2014\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_24_2014\working';
 end
 MD(i).Notes = [];
 
@@ -211,7 +211,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Square Open Field';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_27_2014\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_27_2014\working';
 end
 MD(i).Notes = [];
 
@@ -222,7 +222,7 @@ MD(i).Session = 1;
 MD(i).Env = 'Square Open Field';
 MD(i).Room = '2 Cu 201B';
 if (strcmp(userstr,'Dave'))
-    MD(i).Location = 'E:\GC6f_31\10_29_2014\working';
+    MD(i).Location = 'E:\GCaMP6f_31\10_29_2014\working';
 end
 MD(i).Notes = 'Rotated 90 deg CCW';
 
